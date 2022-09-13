67fafb91cd12f92f68d72f9663837d962f9f7a39
RaphaelNdonga <nkibi53@gmail.com>
2022-09-13 16:15:23 +0300

    changes to deploy page

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -9,11 +9,10 @@ const { ethers } = require("hardhat");
 
 async function main() {
   const NFCFactory = await ethers.getContractFactory("NFC");
-  let dummyOwners = ["0xF5591E14eB99aB51C10ba75DabA7d0D6345293eb", "0x9211cd5a0940FA9F71bcbcF1d45b0EC20Cb62A38"];
-  let dummyUrls = ["https://ipfs.io/1", "https://ipfs.io/2"];
-  const NFC = await NFCFactory.deploy("JKUAT-GRADUATES-2022", "JKUAT Certificate", dummyOwners, dummyUrls);
+  let owners = ["0xF5591E14eB99aB51C10ba75DabA7d0D6345293eb", "0x9211cd5a0940FA9F71bcbcF1d45b0EC20Cb62A38"];
+  let urls = ["https://ipfs.io/1", "https://ipfs.io/2"];
+  const NFC = await NFCFactory.deploy("JKUAT-GRADUATES-2022", "JKUAT Certificate", owners, urls);
   await NFC.deployed();
-  console.log("NFC: ", NFC);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
