6f6a010fe4e599d797b2154bafb480268e61b42e
RaphaelNdonga <nkibi53@gmail.com>
2022-11-14 21:33:00 +0300

    removed nft storage

--- nfc-dapp/pages/index.js
+++ nfc-dapp/pages/index.js
@@ -7,11 +7,9 @@ import NFCJSON from './NFC.json';
 import Web3 from "web3";
 import { create } from "ipfs-http-client";
 import * as d3 from "d3";
-import { NFTStorage } from "nft.storage";
 import ClipLoader from "react-spinners/ClipLoader"
 
 export default function Home() {
-  const NFT_STORAGE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNENTc3RTQwODAwRDM2YkYxNUI0Qzk0ODZFZmE4N2I4MEFGM0VBNjAiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2Mjc5NzY3MzE2NCwibmFtZSI6Im5mYyJ9.0TgVJUuFUv-2Ff4bnDVKmYurzY0ffGi1xuIyLiotqC4'
   const projectId = "2EOa6dNYowZzjK9u2lexz7yqWLA";
   const projectSecret = "cda9ecceee45c322470cdde0596235cb";
   const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
@@ -135,7 +133,7 @@ export default function Home() {
     }
   }
   async function saveToIPFSAndMint(studentArray) {
-    const nftStorage = new NFTStorage({ token: NFT_STORAGE_KEY });
+    console.log("hey hey heeey");
     let owners = [];
     let urls = [];
     for (let i = 0; i < studentArray.length; i++) {
@@ -143,13 +141,12 @@ export default function Home() {
       let svgLink = `<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 500 350'><style>.base { fill: white; font-family: serif; font-size: 16px; }</style><rect width='100%' height='100%' fill='black' /><text x='10%' y='40%' class='base' dominant-baseline='middle' text-anchor='start'>Name: ${studentData.name}</text><text x='10%' y='50%' class='base' dominant-baseline='middle' text-anchor='start'>Degree awarded: ${studentData.course}</text><text x='10%' y='60%' class='base' dominant-baseline='middle' text-anchor='start'>Honors: ${studentData.honors}</text></svg>`;
       let imageData = btoa(svgLink);
       let imageLink = `data:image/svg+xml;base64,${imageData}`;
-      console.log("image link: ", imageLink);
+      console.log("imag link: ", imageLink);
       const date = new Date();
       const day = date.getDay();
       const dateNumber = date.getDate();
       const month = date.getMonth();
       const year = date.getFullYear();
-      const blob = new Blob([svgLink], { type: "image/svg+xml" });
       const ipfsJSON = {
         name: "JKUAT GRADUATE CERTIFICATE",
         image: imageLink,
@@ -168,25 +165,6 @@ export default function Home() {
           },
         ]
       }
-      const nftStorageResult = await nftStorage.store({
-        name: "JKUAT GRADUATE CERTIFICATE",
-        image: blob,
-        description: `This is to certify that ${studentData.name} having satisfied all the requirements for the degree of ${studentData.course} ${studentData.honors} was admitted to the degree at a congregation held at this university on ${dayArray[day]} ${dateNumber} of ${monthArray[month]} in the year ${year}`,
-        properties: [
-          {
-            trait_type: "name",
-            value: studentData.name
-          },
-          {
-            trait_type: "honors",
-            value: studentData.honors
-          }, {
-            trait_type: "course",
-            value: studentData.course
-          },
-        ]
-      })
-      console.log("NFT Storage result: ", nftStorageResult);
       console.log("ipfs JSON", ipfsJSON);
       const ipfsFile = await client.add(JSON.stringify(ipfsJSON));
       const ipfsLink = `https://ipfs.io/ipfs/${ipfsFile.path}`
